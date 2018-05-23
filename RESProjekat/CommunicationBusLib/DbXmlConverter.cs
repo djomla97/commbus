b1bf38c5868002984579bb772fe37c7cbc3f9afc
mladjo97 <mladjo97@localhost>
2018-05-23 20:17:54 +0200

    Added DbXmlConverter and some interfaces

---
+++ RESProjekat/CommunicationBusLib/DbXmlConverter.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using SharedResources;
+using System.Text;
+using System.Xml;
+
+namespace CommunicationBusLib
+{
+    public class DbXmlConverter
+    {
+        public string ConvertToXml(IResponse response)
+        {
+            string jsonResponse = JsonConvert.SerializeObject(response);
+            XmlDocument xmlResponse = (XmlDocument)JsonConvert.DeserializeXmlNode(jsonResponse);
+
+            return xmlResponse.ToString();
+        }
+
+        public string ConvertFromXml(string request)
+        {
+            // kako izgleda GET /resource/1 ???
+            
+            /*
+            StringBuilder sqlQuery = new StringBuilder();
+
+            // METHOD
+            int firstIndexOfVerb = request.IndexOf("<verb>");
+            int lastIndexOfVerb = request.IndexOf("</verb>");
+
+            string method = request.Substring(firstIndexOfVerb + 6, lastIndexOfVerb - firstIndexOfVerb);
+
+            // WHERE
+            int firstIndexOfNoun = request.IndexOf("<noun>");
+            int lastIndexOfNoun = request.IndexOf("</noun>");
+
+            string whereQuery = request.Substring(firstIndexOfNoun + 6, lastIndexOfNoun - firstIndexOfNoun);
+
+            string[] whereQueryParsed = whereQuery.Split('/');
+            
+            
+            switch (method)
+            {
+                case "GET":
+                    sqlQuery.Append("SELECT ");
+                    break;
+                case "POST":
+                    sqlQuery.Append("INSERT INTO resources");
+                    break;
+                case "PATCH":
+                    sqlQuery.Append("UPDATE resources SET ");
+                    break;
+                case "DELETE":
+                    sqlQuery.Append("DELETE FROM resources ");
+                    break;
+            }
+            */
+
+            return "";
+        }
+    }
+}
