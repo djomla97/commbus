69b2a82903692ef3656d9e8a867aae5f0da42e97
sasa96 <sasa96@localhost>
2018-05-23 20:18:16 +0200

    Added JsonToXmlConverter and XmlToJsonConverter

---
+++ RESProjekat/CommunicationBusLib/JsonXmlConverter.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CommunicationBusLib
+{
+    public class JsonXmlConverter
+    {
+        public string ConvertFromXml(string xml)
+        {
+            string json = "";
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.LoadXml(xml);
+
+            json = JsonConvert.SerializeXmlNode(xmlDocument);
+
+            return json;
+        }
+        public string ConvertToXml(string json)
+        {
+            XmlDocument xmlDocument = (XmlDocument)JsonConvert.DeserializeXmlNode(json);
+            string xml = xmlDocument.ToString();
+
+            return xml;
+        }
+    }
+}
