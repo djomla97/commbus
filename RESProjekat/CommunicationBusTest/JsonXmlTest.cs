10a426b98075ed246024e70afb52bce4316e3d20
sasa96 <sasa96@localhost>
2018-06-01 11:43:22 +0200

    Added tests

--- RESProjekat/CommunicationBusTest/JsonXmlTest.cs
+++ RESProjekat/CommunicationBusTest/JsonXmlTest.cs
@@ -15,10 +15,12 @@ namespace CommunicationBusTest
         private static string OcekivaniXml = "<request>\r\n  <verb>GET</verb>\r\n  <noun>/resurs/1</noun>\r\n  <query>name='pera';type=1</query>\r\n  <fields>id;name;surname</fields>\r\n</request>";
         private static string OcekivaniJson = "{\"request\":{\"verb\":\"GET\",\"noun\":\"/resurs/1\",\"query\":\"name='pera';type=1\",\"fields\":\"id;name;surname\"}}";
 
+        private static string OcekivaniXml2 = "<request>\r\n  <verb>POST</verb>\r\n  <noun>/resurs</noun>\r\n  <query>name='pera';type=1</query>\r\n</request>";
+
 
         [Test]
         [TestCase("<request><verb>GET</verb><noun>/resurs/1</noun><query>name='pera';type=1</query><fields>id;name;surname</fields></request>")]
-        public void CovenrtFromXmlDobar(string xml)
+        public void ConvertFromXmlDobar(string xml)
         {
             JsonXmlConverter jsonXml = new JsonXmlConverter();
             string json = jsonXml.ConvertFromXml(xml);
@@ -29,12 +31,23 @@ namespace CommunicationBusTest
         //dovrsiti
         [Test]
         [TestCase("{\"request\":{\"verb\":\"GET\",\"noun\":\"/resurs/1\",\"query\":\"name='pera';type=1\",\"fields\":\"id;name;surname\"}}")]
-        public void CovenrtToXmlDobar(string json)
+        public void ConvertToXmlDobar(string json)
         {
             JsonXmlConverter xmlConverter = new JsonXmlConverter();
             string xml = xmlConverter.ConvertToXml(json);
 
             Assert.AreEqual(xml, OcekivaniXml);
-        } 
+        }
+
+
+        [Test]
+        [TestCase("{\"request\":{\"verb\":\"POST\",\"noun\":\"/resurs\",\"query\":\"name='pera';type=1\"}}")]
+        public void ConverttToXmlDobar2(string json)
+        {
+            JsonXmlConverter xmlConverter = new JsonXmlConverter();
+            string xml = xmlConverter.ConvertToXml(json);
+
+            Assert.AreEqual(xml, OcekivaniXml2);
+        }
     }
 }
