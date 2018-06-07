fa155567f56eb8af30ad93aea4d456fa868564fc
mladjo97 <mladjo97@localhost>
2018-06-07 15:08:37 +0200

    Changed Program.cs for ClientConsoleApp to display available commands

--- RESProjekat/ClientConsoleApp/Program.cs
+++ RESProjekat/ClientConsoleApp/Program.cs
@@ -10,20 +10,35 @@ namespace ClientConsoleApp
             IWebClient webClient = new WebClient();
 
             string request = string.Empty;
-            while(true)
+            while (true)
             {
-                Console.WriteLine("Enter request: ");
+                Console.WriteLine("Request formats:");
+                Console.WriteLine("\t GET /tableName/{id} {\"key\"=\"value\", \"connectedTo\"=\"values\", \"fields\"=\"values\"}");
+                Console.WriteLine("\t ID is optional, but if you enter it, further queries will be ignored \n");
+
+                Console.WriteLine("\t POST /tableName {\"key\"=\"value\", \"key\"=\"value\" ... }");
+                Console.WriteLine("\t ID is incremental and unique, no need to pass it \n");
+
+                Console.WriteLine("\t PATCH /tableName/id {\"key\"=\"newValue\", \"key\"=\"newValue\" ... }");
+                Console.WriteLine("\t Update entity with id \n");
+
+                Console.WriteLine("\t DELETE /tableName/id");
+                Console.WriteLine("\t Deletes entity with id \n");
+
+                Console.WriteLine("\t Tables: resource (GET, POST, PATCH, DELETE), connection (POST, DELETE) \n");
+
+                Console.Write("Enter request (exit to quit): ");
                 request = Console.ReadLine();
 
                 if (request.Equals("exit"))
                     break;
 
-                Console.WriteLine(webClient.SendRequest(request));
+                Console.WriteLine(Environment.NewLine + webClient.SendRequest(request));
+
+                Console.WriteLine("\nPress any key to enter another request.");
+                Console.ReadKey();
+                Console.Clear();
             }
-
-            // string jsonQuery = "{ \" name\" = \" pera \", \" type\" = \" 1 \", \" connectedTo\" = \" 3, 5 \",  \" connectedType\" = \" 3, 4, 5 \", \" fields\" = \" id, title, name \"}";
-
-            //Console.WriteLine(webClient.SendRequest("GET /resource" + " " + jsonQuery));
 
             Console.ReadKey();
         }
